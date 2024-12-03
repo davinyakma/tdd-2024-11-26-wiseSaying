164a6cdb021905c426aa00a8e0876b32a4e40f88
davinyakma <poohcdv3652@naver.com>
2024-12-03 17:44:48 +0900

    feat: TestUtil step5 completed t7()_b_WiseSayingRepository

--- src/main/java/com/programmers/domain/wiseSaying/service/WiseSayingService.java
+++ src/main/java/com/programmers/domain/wiseSaying/service/WiseSayingService.java
@@ -1,26 +1,26 @@
 package com.programmers.domain.wiseSaying.service;
 
 import com.programmers.domain.wiseSaying.entity.WiseSaying;
+import com.programmers.domain.wiseSaying.repository.WiseSayingRepository;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class WiseSayingService {
-    private List<WiseSaying> wiseSayings;
-    private int lastId;
+    private final WiseSayingRepository wiseSayingRepository;
 
     public WiseSayingService() {
-        this.wiseSayings = new ArrayList<>();
-        this.lastId = 0;
+        this.wiseSayingRepository = new WiseSayingRepository();
     }
 
     public WiseSaying add(String content, String author) {
-        WiseSaying wiseSaying = new WiseSaying(++lastId, content, author);
-        wiseSayings.add(wiseSaying);
+        WiseSaying wiseSaying = new WiseSaying(0, content, author);
+
+        wiseSayingRepository.save(wiseSaying);
+
         return wiseSaying;
     }
 
     public List<WiseSaying> findAll() {
-        return wiseSayings;
+        return wiseSayingRepository.findAll();
     }
 }
