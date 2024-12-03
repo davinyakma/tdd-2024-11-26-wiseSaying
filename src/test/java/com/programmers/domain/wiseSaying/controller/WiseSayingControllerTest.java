fd039812e7eb1bd7eaa089736e5141c97f0e6cad
davinyakma <poohcdv3652@naver.com>
2024-12-03 12:12:36 +0900

    feat: TestUtil step1_'order) ' t2()_r

--- src/test/java/com/programmers/domain/wiseSaying/controller/WiseSayingControllerTest.java
+++ src/test/java/com/programmers/domain/wiseSaying/controller/WiseSayingControllerTest.java
@@ -27,4 +27,24 @@ public class WiseSayingControllerTest {
         assertThat(output)
                 .contains("== 명언 앱 ==");
     }
+
+    @Test
+    @DisplayName("명령) ")
+    public void t2() {
+        Scanner scanner = TestUtil.getScanner("""
+                목록
+                종료
+                """);
+        ByteArrayOutputStream outputStream = TestUtil.setOutToByteArray(); //모니터에 출력 안 되도록 설정
+
+        App app = new App(scanner);
+        app.run();
+
+        String output = outputStream.toString();
+
+        TestUtil.clearSetOutToByteArray(outputStream);
+
+        assertThat(output)
+                .contains("명령) ");
+    }
 }
