6404738f3ce882e22231c13d6cd2e397c3efea74
davinyakma <poohcdv3652@naver.com>
2024-12-03 11:45:31 +0900

    feat: create TestUtil step1 t1()_r

---
+++ src/test/java/com/programmers/domain/wiseSaying/controller/WiseSayingControllerTest.java
@@ -0,0 +1,29 @@
+package com.programmers.domain.wiseSaying.controller;
+
+import com.programmers.standard.util.TestUtil;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.util.Scanner;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class WiseSayingControllerTest {
+    @Test
+    @DisplayName("== 명언 앱 ==")
+    public void t1() {
+        Scanner scanner = TestUtil.getScanner("종료");
+        ByteArrayOutputStream outputStream = TestUtil.setOutToByteArray(); //모니터에 출력 안 되도록 설정
+
+        App app = new App(scanner);
+        app.run();
+
+        String output = outputStream.toString();
+
+        TestUtil.clearSetOutToByteArray(outputStream);
+
+        assertThat(output)
+                .contains("== 명언 앱 ==");
+    }
+}
