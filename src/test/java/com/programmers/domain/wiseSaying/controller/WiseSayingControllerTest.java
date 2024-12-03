30aaddbeb55b5b5fe5368eb78b47c3423e93b0ab
davinyakma <poohcdv3652@naver.com>
2024-12-03 14:43:09 +0900

    feat: TestUtil step2 registration t4()_r

--- src/test/java/com/programmers/domain/wiseSaying/controller/WiseSayingControllerTest.java
+++ src/test/java/com/programmers/domain/wiseSaying/controller/WiseSayingControllerTest.java
@@ -4,8 +4,6 @@ import com.programmers.AppTest;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import java.util.Arrays;
-
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class WiseSayingControllerTest {
@@ -37,11 +35,21 @@ public class WiseSayingControllerTest {
                 목록
                 """);
 
-        System.out.println(
-                Arrays.toString(output.split("명령\\)"))
-        );
-
         String[] split = output.split("명령\\)"); //'명령) '을 기준으로 split(나누면) 그룹이 4개가 생김.
         assertThat(split).hasSize(4);
+    }
+
+    @Test
+    @DisplayName("등록을 입력하면 내용과 작가를 입력받는다.")
+    public void t4() {
+        String output = AppTest.run("""
+                등록
+                현재를 사랑하라.
+                작자미상
+                """);
+
+        assertThat(output)
+                .contains("명언 : ")
+                .contains("작가 : ");
     }
 }
