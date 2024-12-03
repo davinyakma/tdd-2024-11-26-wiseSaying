406490df9c33b739fd2878da8d83a119f43502b1
davinyakma <poohcdv3652@naver.com>
2024-12-03 12:49:59 +0900

    feat: TestUtil step1_'order) ' multiple times t3()_r

--- src/test/java/com/programmers/domain/wiseSaying/controller/WiseSayingControllerTest.java
+++ src/test/java/com/programmers/domain/wiseSaying/controller/WiseSayingControllerTest.java
@@ -27,4 +27,17 @@ public class WiseSayingControllerTest {
         assertThat(output)
                 .contains("명령) ");
     }
+
+    @Test
+    @DisplayName("명령을 2번 이상 입력할 수 있습니다.")
+    public void t3() {
+        String output = AppTest.run("""
+                목록
+                목록
+                종료
+                """);
+
+        String[] split = output.split("명령\\) "); //'명령) '을 기준으로 split(나누면) 그룹이 4개가 생김.
+        assertThat(split).hasSize(4);
+    }
 }
