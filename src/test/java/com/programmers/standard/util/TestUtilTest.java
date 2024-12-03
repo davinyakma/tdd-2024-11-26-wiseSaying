5a08160c07e04ca9790dfedd1eee9d3165f9af34
davinyakma <poohcdv3652@naver.com>
2024-12-03 11:26:04 +0900

    feat: create TestUtil.setOutToByteArray() t2()_r

--- src/test/java/com/programmers/standard/util/TestUtilTest.java
+++ src/test/java/com/programmers/standard/util/TestUtilTest.java
@@ -3,13 +3,14 @@ package com.programmers.standard.util;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.io.ByteArrayOutputStream;
 import java.util.Scanner;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class TestUtilTest {
     @Test
-    @DisplayName("TestUtil.getScanner()")
+    @DisplayName("TestUtil.getScanner()") //ByteArrayInputStream없이 Scanner 생성
     public void t1() {
         Scanner scanner = TestUtil.getScanner("""
                 등록
@@ -25,4 +26,17 @@ public class TestUtilTest {
         assertThat(author).isEqualTo("이순신");
     }
 
+    @Test
+    @DisplayName("TestUtil.setOutToByteArray()")
+    public void t2() {
+        ByteArrayOutputStream byteArrayOutputStream = TestUtil.setOutToByteArray(); //모니터로 출력 안됨.
+
+        System.out.println("2 / 이순신 / 나의 죽음을 적들에게 알리지 말라!");
+
+        String out = byteArrayOutputStream.toString().trim();
+        TestUtil.clearSetOutToByteArray(byteArrayOutputStream); //화면에 보이게끔 clear함.
+
+        assertThat(out).isEqualTo("2 / 이순신 / 나의 죽음을 적들에게 알리지 말라!");
+        System.out.println("이제는 화면에 출력됩니다.");
+    }
 }
